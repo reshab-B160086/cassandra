edee0c6747c58f07b881e2bd307de357507da4db
reshab@nitsk <32607175+reshab-B160086@users.noreply.github.com>
2022-06-22 11:08:26 +0530

    Update cassandraFetch.java

--- cassandraFetch.java
+++ cassandraFetch.java
@@ -1,4 +1,6 @@
- public List<ConversationDetail> getConversationDetailByCreateDateAndCmId(Timestamp startDateTimestamp, Timestamp endDateTimestamp, int cmId) {
+// this function queries cassandra based on start date and end date. Partition key is each day. so for 1 month data 30 async queries are send. 
+// function works well until 1 thread is invoking this method but if more threads come then the time delays for all threads.
+public List<ConversationDetail> getConversationDetailByCreateDateAndCmId(Timestamp startDateTimestamp, Timestamp endDateTimestamp, int cmId) {
 
         LocalDate startDate = startDateTimestamp.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate endDate = endDateTimestamp.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
@@ -6,14 +8,14 @@
         List<ConversationDetail> results = new ArrayList<>();
         List<ResultSetFuture> futures = new ArrayList<>();
         try {
+         // Async queries begins for each day as partitoion key.
             for (LocalDate date = startDate.plusDays(1); date.isBefore(endDate); date = date.plusDays(1)) {
                 ResultSetFuture resultSetFuture = session.executeAsync(findByCreateDayAndCmId.bind(date, cmId).setReadTimeoutMillis(20000));
                 futures.add(resultSetFuture);
             }
             futures.add(session.executeAsync(findByCreateDayAndCmIdAndTimestampGreaterThanEqual.bind(startDate, cmId, startDateTimestamp).setReadTimeoutMillis(20000)));
             futures.add(session.executeAsync(findByCreateDayAndCmIdAndTimestampLessThanEqual.bind(endDate, cmId, endDateTimestamp).setReadTimeoutMillis(20000)));
-
-            Instant time1 = Instant.now();
+        // Async queries ends.
             for (ResultSetFuture future : futures) {
                 try {
                     ResultSet rows = future.getUninterruptibly();
@@ -21,7 +23,6 @@
                     while (it.hasNext()){
                         results.add(it.next());
                     }
-                    Instant end = Instant.now();
                 }catch (Exception e){
                     System.out.println("Exception1 " + e.getMessage());
                 }
